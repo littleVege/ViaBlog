3950aebcf5c7daf0801cbf229beaa6587d0024af
littleVege <caidroid713@gmail.com>
2014-12-24 00:49:33 +0800

    init create post js

--- public/js/admin/controller.js
+++ public/js/admin/controller.js
@@ -1,16 +1,38 @@
-AdminModule.controller('PostListController',['$scope','$http',function($scope,$http) {
-    $scope.posts = [
-        {title:'name',abstract:'title~~~~~~~~~~',pid:1}
-    ];
-    $scope.currentPost = null;
-    $scope.getContent = function(post) {
-        $scope.currentPost = post;
-        $http.get('/p/'+post.pid).success(function(data) {
-            $scope.currentPost.content = data;
+AdminModule
+    .controller('PostListController',['$scope','$http',function($scope,$http) {
+        $scope.posts = [
+            {title:'name',abstract:'title~~~~~~~~~~',pid:1}
+        ];
+        $scope.currentPost = null;
+        $scope.getContent = function(post) {
+            $scope.currentPost = post;
+            $http.get('/p/'+post.pid).success(function(data) {
+                $scope.currentPost.content = data;
+            });
+        }
+    }])
+    .controller('TabsController',['$scope',function($scope) {
+        $scope.activeTab = null;
+        $scope.tabs=[];
+    }])
+    .controller("CreatePostController",['$scope',"$sce",function($scope,$sce) {
+        // The ui-codemirror option
+        $scope.cmOption = {
+            lineNumbers: false,
+            indentWithTabs: true,
+            theme:'monokai',
+            mode:"markdown"
+        };
+        $scope.mdContent="";
+        $scope.title = "";
+
+        $scope.$watch('mdContent',function(val) {
+            $scope.htmlContent = marked(val);
         });
-    }
-}]);
-AdminModule.controller('TabsController',['$scope',function($scope) {
-    $scope.activeTab = null;
-    $scope.tabs=[];
-}]);
+
+        $scope.asHtml = function(html) {
+            return $sce.trustAsHtml(html);
+        };
+
+        $scope.htmlContent = "";
+    }]);
---
+++ routes/md.js
@@ -0,0 +1,7 @@
+/**
+ * Created by little_vege on 2014/12/23.
+ */
+var exports = module.exports;
+exports.toMd = function (req,res,next) {
+
+};
